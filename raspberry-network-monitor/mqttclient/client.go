1663b2ee5fcfc9c80199357941498ea53c7a7f9c
Eldio Santos Junior <eldiosantos@gmail.com>
2020-05-08 21:42:54 +0000

    Changed the published value model...

--- raspberry-network-monitor/mqttclient/client.go
+++ raspberry-network-monitor/mqttclient/client.go
@@ -34,7 +34,7 @@ func SendPingResponse(p network.PingResponse, cfg config.MQTTConfig) {
 	}
 	fmt.Println("Sample Publisher Started")
 	fmt.Println("---- doing publish ----")
-	token := client.Publish(cfg.Topic, cfg.Qos, false, serialize(p))
+	token := client.Publish(cfg.Topic, cfg.Qos, false, serialize(p.Convert()))
 	token.Wait()
 
 	client.Disconnect(250)
--- raspberry-network-monitor/network/model.go
+++ raspberry-network-monitor/network/model.go
@@ -20,3 +20,50 @@ type PingResponse struct {
 	Host          string
 	ExecutionTime time.Time
 }
+
+/*
+PingResponseTO is a converted type to publish
+*/
+type PingResponseTO struct {
+	AvgTime int64
+	MinTime int64
+	MaxTime int64
+	Jitter  int64
+
+	PacketsSent     int
+	PacketsReceived int
+
+	ResponseTimes []int64
+	Host          string
+	ExecutionTime time.Time
+}
+
+/*
+Convert converts the respon se to a TO value
+*/
+func (r *PingResponse) Convert() PingResponseTO {
+	return PingResponseTO{
+		AvgTime:         convertToMili(r.AvgTime),
+		MinTime:         convertToMili(r.MinTime),
+		MaxTime:         convertToMili(r.MaxTime),
+		Jitter:          r.Jitter,
+		PacketsSent:     r.PacketsSent,
+		PacketsReceived: r.PacketsReceived,
+		ResponseTimes:   convertAllToMili(r.ResponseTimes),
+		Host:            r.Host,
+		ExecutionTime:   r.ExecutionTime,
+	}
+}
+
+
+func convertToMili(d time.Duration) int64 {
+	return int64(d / time.Millisecond)
+}
+
+func convertAllToMili(ds []time.Duration) []int64 {
+	var parsedValues []int64
+	for _, d := range ds {
+		parsedValues = append(parsedValues, int64(d / time.Millisecond))
+	}
+	return parsedValues
+}
